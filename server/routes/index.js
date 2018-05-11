eb9034c38554cfc5eca43d0b9e9655d087675f7b
frediansimanjuntak <fredian.pc@gmail.com>
2018-05-11 14:48:10 +0700

    minor fix route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var todo_route_1 = require("../api/todo/route/todo-route");
 var user_route_1 = require("../api/user/route/user-route");
+var quiz_route_1 = require("../api/quiz/route/quiz-route");
+var interest_route_1 = require("../api/interest/route/interest-route");
 var Routes = /** @class */ (function () {
     function Routes() {
     }
     Routes.init = function (app, router) {
         todo_route_1.TodoRoutes.init(router);
         user_route_1.UserRoutes.init(router);
+        quiz_route_1.QuizRoutes.init(router);
+        interest_route_1.InterestRoutes.init(router);
         app.use('/auth', require('../auth').default);
         app.use("/", router);
     };
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,15 +1,20 @@
 import * as express from "express";
 import {TodoRoutes} from "../api/todo/route/todo-route";
 import {UserRoutes} from "../api/user/route/user-route";
+import {QuizRoutes} from "../api/quiz/route/quiz-route";
+import {InterestRoutes} from "../api/interest/route/interest-route";
 
 
 export class Routes {
    static init(app: express.Application, router: express.Router) {
      TodoRoutes.init(router);
-     UserRoutes.init(router);
+     UserRoutes.init(router);     
+     QuizRoutes.init(router);
+     InterestRoutes.init(router);
 
     
      app.use('/auth', require('../auth').default);
      app.use("/", router);
    }
 }
+  
