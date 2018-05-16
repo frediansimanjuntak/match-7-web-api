f2354871734f95edfcd4b0d3349801c88e13566e
Unknown <fredian.pc@gmail.com>
2018-05-16 14:44:55 +0700

    remove unused file

--- server/config/environment/shared.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-// List of user roles
-//   userRoles: ["guest", "user", "admin", "super-admin", "master"]
-};
-//# sourceMappingURL=shared.js.map
